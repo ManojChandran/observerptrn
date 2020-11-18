e5a7d8bea6bb1c593f2bbe6332714b76615b0b4a
Manojchandran <manoj.meparambu@gmail.com>
2020-11-18 20:38:53 +0530

    updated test

--- observerptrn_test.go
+++ observerptrn_test.go
@@ -1,9 +1,19 @@
 package observerptrn
 
 import (
+	"fmt"
 	"testing"
 )
 
+type TestObserver struct {
+	ID      int
+	Message string
+}
+
+func (p *TestObserver) Notify(m string) {
+	fmt.Printf("Observer %d: message '%s' received \n", p.ID, m)
+	p.Message = m
+}
 func TestObserverptrn(t *testing.T) {
 	// Init
 
@@ -11,4 +21,18 @@ func TestObserverptrn(t *testing.T) {
 
 	// Validation
 
+	testObserver1 := &TestObserver{1, ""}
+	testObserver2 := &TestObserver{2, ""}
+	testObserver3 := &TestObserver{3, ""}
+	publisher := Publisher{}
+
+	t.Run("Subscribe", func(t *testing.T) {
+		publisher.Subscribe(testObserver1)
+		publisher.Subscribe(testObserver2)
+		publisher.Subscribe(testObserver3)
+
+		if len(publisher.SubscribersList) != 3 {
+			t.Fail()
+		}
+	})
 }
